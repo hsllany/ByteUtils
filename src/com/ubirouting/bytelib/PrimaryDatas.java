bb4444101211e5ad44518a8cdcf2b2553af73ecc
hsllany <hsllany@163.com>
2016-06-29 13:05:40 +0800

    fixing bugs of primary type convertor

--- src/com/ubirouting/bytelib/PrimaryDatas.java
+++ src/com/ubirouting/bytelib/PrimaryDatas.java
@@ -1,7 +1,5 @@
 package com.ubirouting.bytelib;
 
-import java.nio.ByteBuffer;
-
 /**
  * primary type convention utilization
  *
@@ -12,25 +10,18 @@ public class PrimaryDatas {
 
     public static byte[] s2b(short x) {
         byte[] bytes = new byte[2];
-
-        bytes[0] = (byte) (x >> 8);
-        bytes[1] = (byte) (x);
-
+        s2b(x, bytes, 0);
         return bytes;
     }
 
     public static void s2b(short x, byte[] bytes, int start) {
         bytes[start] = (byte) (x >> 8);
-        bytes[start] = (byte) (x);
+        bytes[start + 1] = (byte) (x);
     }
 
     public static byte[] i2b(int x) {
-        int size = 4;
-        byte[] y = new byte[size];
-        y[3] = (byte) x;
-        y[2] = (byte) (x >> 8);
-        y[1] = (byte) (x >> 16);
-        y[0] = (byte) (x >> 24);
+        byte[] y = new byte[4];
+        i2b(x, y, 0);
         return y;
     }
 
@@ -43,10 +34,7 @@ public class PrimaryDatas {
 
     public static byte[] l2b(long x) {
         byte[] byteNum = new byte[8];
-        for (int ix = 0; ix < 8; ++ix) {
-            int offset = 64 - (ix + 1) * 8;
-            byteNum[ix] = (byte) ((x >> offset) & 0xff);
-        }
+        l2b(x, byteNum, 0);
         return byteNum;
     }
 
@@ -57,31 +45,26 @@ public class PrimaryDatas {
         }
     }
 
-    public static long b2l(byte[] byteNum, int start) {
-        long num = 0;
-        for (int ix = 0; ix < 8; ++ix) {
-            num <<= 8;
-            num |= (byteNum[start + ix] & 0xff);
-        }
-        return num;
-    }
 
     public static byte[] f2b(float f) {
-        return ByteBuffer.allocate(4).putFloat(f).array();
+        byte[] bytes = new byte[4];
+        f2b(f, bytes, 0);
+        return bytes;
     }
 
     public static void f2b(float f, byte[] bytes, int start) {
-        byte[] bytes1 = ByteBuffer.allocate(4).putFloat(f).array();
-        System.arraycopy(bytes, start, bytes1, 0, 4);
+        i2b(Float.floatToIntBits(f), bytes, start);
     }
 
     public static void d2b(double x, byte[] bytes, int start) {
-        byte[] bytes1 = ByteBuffer.allocate(Double.BYTES).putDouble(x).array();
-        System.arraycopy(bytes, start, bytes1, 0, 8);
+        long l = Double.doubleToLongBits(x);
+        l2b(l, bytes, start);
     }
 
     public static byte[] d2b(double x) {
-        return ByteBuffer.allocate(Double.BYTES).putDouble(x).array();
+        byte[] bytes = new byte[8];
+        d2b(x, bytes, 0);
+        return bytes;
     }
 
     /**
@@ -100,6 +83,15 @@ public class PrimaryDatas {
         return r;
     }
 
+    public static long b2l(byte[] byteNum, int start) {
+        long num = 0;
+        for (int ix = 0; ix < 8; ++ix) {
+            num <<= 8;
+            num |= (byteNum[start + ix] & 0xff);
+        }
+        return num;
+    }
+
     /**
      * convert bytes which was converted by ShiTu ByteArrayConvertor.s2b, to
      * short
@@ -109,11 +101,7 @@ public class PrimaryDatas {
      * @return
      */
     public final static short b2s(byte[] bytes, int start) {
-        short r = 0;
-
-        r = (short) ((bytes[start] << 8) | (bytes[start + 1] & 0xff));
-
-        return r;
+        return (short) ((bytes[start] << 8) | (bytes[start + 1] & 0xff));
     }
 
     /**
@@ -126,6 +114,11 @@ public class PrimaryDatas {
     public final static float b2f(byte[] bytes, int start) {
         int i = b2i(bytes, start);
         return Float.intBitsToFloat(i);
+    }
+
+    public final static double b2d(byte[] bytes, int start) {
+        long l = b2l(bytes, start);
+        return Double.longBitsToDouble(l);
     }
 
     /**
